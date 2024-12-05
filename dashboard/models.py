7a63e74ef042fa30ade1cb0f98fba8d6b4451d83
cboncales <clarkboncales115@gmail.com>
2024-12-05 10:53:53 +0800

    feat: created Message model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    name = models.CharField(max_length=100)
+    score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
