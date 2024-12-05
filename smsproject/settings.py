bf40d7eead7347141d8b945f0a530258f10e2c6f
cboncales <clarkboncales115@gmail.com>
2024-12-05 10:31:59 +0800

    created new file

--- smsproject/settings.py
+++ smsproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!4o)o0%rv$3w!90xphav0im6+ydmf!%iwj9x*0y7+0qd0ku@_f'
+SECRET_KEY = 'django-insecure-y9p!ush6)yn6a1+@)gkyg_0el3&o86y4xd2!ys$b@go3rls6cm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground'
+    'dashboard.apps.DashboardConfig',
 ]
 
 MIDDLEWARE = [
