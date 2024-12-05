e793b30f0d5ace3df67c8853308122cbf7e96fa4
cboncales <clarkboncales115@gmail.com>
2024-12-05 10:58:42 +0800

    feat: added name and score in Message

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Message
 
 # Register your models here.
-admin.site.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'score']
+
+admin.site.register(Message, MessageAdmin)
